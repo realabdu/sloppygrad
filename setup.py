13ed01dc7055bd9546d0f2311dc40f781dd682a5
realabdu <abdullah.rm.2020@gmail.com>
2023-09-04 20:07:30 +0300

    added setup.py for installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+        name="sloppygrad",
+        version="0.0.1",
+        description="sloppygrad a sloppy,cheap version of micorgrad an autograd engine",
+        author="abdullah mosaibah",
+        author_email="abdullah.rm.2020@gmail.com",
+        install_requires=[
+            "numpy"
+            ]
+        )
